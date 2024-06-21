31741adf6ac69c0d1757367dde62b1dda809099e
Sheymor <joseacvz81@gmail.com>
2024-06-20 23:22:06 -0400

    migrations: update models configuration

--- SimpleShop/Migrations/20240621032126_update model configuration.Designer.cs
+++ SimpleShop/Migrations/20240621032126_update model configuration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleShop.Context;
 
@@ -10,9 +11,11 @@ using SimpleShop.Context;
 namespace SimpleShop.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240621032126_update model configuration")]
+    partial class updatemodelconfiguration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
---
+++ SimpleShop/Migrations/20240621032126_update model configuration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatemodelconfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ItemId",
+                table: "Items",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "d658e832-e199-437e-ab21-de4cd84c861a",
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "09cd19fb-76b7-4be7-9ca1-f9ce36291a2a",
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ItemId",
+                table: "Items",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldDefaultValue: "d658e832-e199-437e-ab21-de4cd84c861a");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ClientId",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldDefaultValue: "09cd19fb-76b7-4be7-9ca1-f9ce36291a2a");
+        }
+    }
+}
