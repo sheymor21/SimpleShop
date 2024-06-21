b21f5be0f22c451ac966c9ac62bd8f9abcb1c26f
Sheymor <joseacvz81@gmail.com>
2024-06-21 01:33:33 -0400

    migrations: configurations migrations

---
+++ SimpleShop/Migrations/20240621053040_restore the configuration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class restoretheconfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ItemId",
+                table: "Items",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldDefaultValue: "d658e832-e199-437e-ab21-de4cd84c861a");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldDefaultValue: "09cd19fb-76b7-4be7-9ca1-f9ce36291a2a");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ItemId",
+                table: "Items",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "d658e832-e199-437e-ab21-de4cd84c861a",
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "09cd19fb-76b7-4be7-9ca1-f9ce36291a2a",
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
